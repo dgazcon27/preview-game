475f01dd858fcf25c3c471f183add7643770446e
Daniel Gazcon <daniel.gazcon27@gmail.com>
2021-05-20 21:39:05 -0400

    fix resolution option letter from 300px to 485px

--- src/hooks/useSetBackGround.js
+++ src/hooks/useSetBackGround.js
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 export function useSetBackGround(background, color = "white") {
   useEffect(() => {
     document.body.style.backgroundImage = `url(${background})`;
+    document.documentElement.style.backgroundImage = `url(${background})`;
     document.body.style.backgroundColor = color;
   });
 }
